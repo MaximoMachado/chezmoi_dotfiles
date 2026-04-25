697f04ffbe1252c067d1ffa9ee55f8a8d2473e3b
Maximo Machado <maximomachado246@gmail.com>
2026-04-25 02:21:24 -0700

    Update fish and git

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -9,9 +9,9 @@ alias fd="fdfind"
 alias zel="zellij"
 
 if type -q nvim
-    set -gx EDITOR nvim
+    set -x EDITOR=nvim
 else
-    set -gx EDITOR vim
+    set -x EDITOR=vim
 end
 
 ssh-add $HOME/.ssh/golden-finch
@@ -21,12 +21,11 @@ fish_add_path /home/maximo/.opencode/bin
 fish_add_path /usr/local/cuda/bin
 fish_add_path $HOME/build/llama.cpp/build/bin
 
-set -gx LD_LIBRARY_PATH /usr/local/cuda/lib64 $LD_LIBRARY_PATH
+set -x LD_LIBRARY_PATH /usr/local/cuda/lib64 $LD_LIBRARY_PATH
 
 atuin init fish | source
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
-
-end # is-interactive
+end
 
 set -gx PATH "/home/maximo/.pixi/bin" $PATH
 
